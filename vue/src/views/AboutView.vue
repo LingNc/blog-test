3cad664ff916b9874922c279f3184218a3f7638c
LingNc <1020449099@qq.com>
2024-09-13 01:39:11 +0800

    LoginForm.vue

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <LoginForm />
   </div>
 </template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm
+  }
+};
+</script>
